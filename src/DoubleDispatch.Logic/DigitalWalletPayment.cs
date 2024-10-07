2b953035e3ba75fd8175c4966c7de8c3c12eaea0
Ryszard Balcerzak <rysz.b82@gmail.com>
2024-10-08 00:37:57 +0200

    Update concrete payment classes to use base class

--- src/DoubleDispatch.Logic/DigitalWalletPayment.cs
+++ src/DoubleDispatch.Logic/DigitalWalletPayment.cs
@@ -1,31 +1,16 @@
 namespace DoubleDispatch.Logic;
 
-public class DigitalWalletPayment : IPayment
+public class DigitalWalletPayment : Payment
 {
-    public decimal CalculateFee(Region region)
-    {
-        if (region is Europe europe)
-        {
-            return CalculateFeeForRegion(europe);
-        }
-    
-        if (region is NorthAmerica northAmerica)
-        {
-            return CalculateFeeForRegion(northAmerica);
-        }
-    
-        throw new InvalidOperationException("Unsupported region");
-    }
-    
-    private decimal CalculateFeeForRegion(Europe region)
+    protected override decimal CalculateFeeForRegion(Europe region)
     {
         // More complex fee calculation logic is implemented here,
         // but a fixed value is used for brevity
         
         return 1.10m;
     }
-    
-    private decimal CalculateFeeForRegion(NorthAmerica region)
+
+    protected override decimal CalculateFeeForRegion(NorthAmerica region)
     {
         // More complex fee calculation logic is implemented here,
         // but a fixed value is used for brevity
